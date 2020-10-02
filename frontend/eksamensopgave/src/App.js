d09f7061253141b9dec0f549b5b88c5c172b38f0
Jonas-MM <50103068+Jonas-MM@users.noreply.github.com>
2020-10-02 12:07:19 +0200

    Update App.js

--- frontend/eksamensopgave/src/App.js
+++ frontend/eksamensopgave/src/App.js
@@ -1,41 +1,36 @@
-import React from 'react';
+import React from "react";
 
 //routes
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-
-
 //navbar
-import Navbar from './components/Navbar/Navbar';
+import Navbar from "./components/Navbar/Navbar";
 
 //pages
-import Home from './components/Pages/Home/Home';
-import SpecificProduct from './components/Pages/SpecificProduct/SpecificProduct';
-import Prpdukter from './components/Pages/Produkter/Produkter';
-import Login from './components/Pages/Login/Login';
-import Register from './components/Pages/Login/Register';
-import Rediger from './components/Pages/Rediger/Rediger';
-import Kontakt from './components/Pages/Kontakt/Kontakt';
-import Footer from './components/Footer/Footer';
+import Home from "./components/Pages/Home/Home";
+import SpecificProduct from "./components/Pages/SpecificProduct/SpecificProduct";
+import Prpdukter from "./components/Pages/Produkter/Produkter";
+import Login from "./components/Pages/Login/Login";
+import Register from "./components/Pages/Login/Register";
+import Kontakt from "./components/Pages/Kontakt/Kontakt";
 
 function App() {
   return (
-      <BrowserRouter>
-        <div className="App">
-          {/* <Home /> */}
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/produkter" component={Prpdukter} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-            <Route path="/Rediger" component={Rediger} />
-            <Route path="/kontakt" component={Kontakt} />
-            <Route path="/:id" component={SpecificProduct} />
-          </Switch>
-          <Footer/>
-        </div>
-      </BrowserRouter>
+    <BrowserRouter>
+      <div className="App">
+        {/* <Home /> */}
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/produkter" component={Prpdukter} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          {/* <Route path="/side4" component={Side4} /> */}
+          <Route path="/kontakt" component={Kontakt} />
+          <Route path="/:id" component={SpecificProduct} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
